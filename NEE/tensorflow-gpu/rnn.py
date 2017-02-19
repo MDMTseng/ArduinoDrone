e87e81a19a09dc040bb3160b94eebe5e8b963234
MDM Tseng <chiapintseng@gmail.com>
2017-02-18 20:23:54 -0800

    Finish rnn.py

--- NEE/tensorflow-gpu/rnn.py
+++ NEE/tensorflow-gpu/rnn.py
@@ -3,6 +3,16 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from tensorflow.python.ops import rnn, rnn_cell
 import numpy
+import numpy.random as rng
+
+
+hm_epochs = 1300
+hm_batch_size = 1200
+
+input_dim = 1
+output_dim = 1
+time_span = 50
+rnn_size = 50
 
 def sqFunc(arr,start,stop):
     ret=[]
@@ -10,29 +20,35 @@ def sqFunc(arr,start,stop):
         ret.append((0,1)[ele>=start and ele<stop])
     return ret
 
-data_range = [-0.5, 0.5]
+def GenXYSignal(start_t,stop_t,N):
+    t=numpy.linspace(start_t,stop_t,N);
+    X = numpy.asarray(sqFunc(  t,0.0,0.1 ))
+    Y = numpy.asarray(sqFunc(  t,0.1,1.5 )*numpy.sin(numpy.pi*5*t))
+    return X,Y
 
 
-hm_epochs = 1300
-hm_batch_size = 1
+def GenXYSignalSets(randArr,N):
+    counter=0
+    #The randArr is to give random offset of time to offset X,Y
+    for rand in randArr:
+        x,y=GenXYSignal(rand,rand+1,N)
+        if counter == 0:
+            set_x,set_y=x,y
+        else:
+            set_x=numpy.vstack((set_x,x))
+            set_y=numpy.vstack((set_y,y))
+        counter=counter+1
 
-input_dim = 1
-output_dim = 1
-time_span = 50
-rnn_size = 50
 
-t=numpy.linspace(0,1,time_span);
-train_X = numpy.asarray(t-0.5)*5
-train_Y = 0.5*numpy.asarray( \
-              sqFunc( t,0.5,1.1 )* numpy.sin(16*(t-0.5))  \
-             -sqFunc(  t,0,0.5  )* 1 \
-             +sqFunc(  t,0.2,0.8)* (0.5-t) \
-             )
-train_X=train_X.reshape([1,time_span,input_dim])
-train_Y=train_Y.reshape([1,time_span,output_dim])
+    return set_x,set_y
 
-train_X_b = numpy.array([train_X,]* hm_batch_size).reshape([hm_batch_size,time_span,input_dim])
-train_Y_b = numpy.array([train_Y,]* hm_batch_size).reshape([hm_batch_size,time_span,output_dim])
+[train_X_b,train_Y_b]=GenXYSignalSets(-rng.rand(hm_batch_size)/1.3,time_span)
+train_X_b = train_X_b.reshape([hm_batch_size,time_span,input_dim])
+train_Y_b = train_Y_b.reshape([hm_batch_size,time_span,output_dim])
+
+
+
+
 
 
 
@@ -54,12 +70,12 @@ def recurrent_neural_network(x):
     x = tf.split(1, time_span, x)
     print(x[0].get_shape())#(input batch size),1,chunk_size:[n_chunks]
     x = [ tf.reshape(x_id, [-1, input_dim]) for x_id in x ]
-    print(x[0].get_shape())#(input batch size),chunk_size:[n_chunks]
+    print(x[0].get_shape())
 
     lstm_cell = rnn_cell.GRUCell(rnn_size)
     outputs, states = rnn.rnn(lstm_cell, x, dtype=tf.float32)
     print("outputs[-1].get_shape()>>")
-    print(outputs[-1].get_shape())#(input batch size),chunk_size:[last chunk]
+    print(outputs[-1].get_shape())
     output_res = [ tf.nn.tanh(tf.matmul(output,layer['weights']) + layer['biases']) for output in outputs ]
     return output_res
 
@@ -67,15 +83,15 @@ def recurrent_neural_network(x):
 def train_neural_network(x):
     prediction = recurrent_neural_network(x)
     print("prediction[-1].get_shape()>>")
-    print(prediction[-1].get_shape())#(input batch size),chunk_size:[last chunk]
+    print(prediction[-1].get_shape())
 
     packed_pred=tf.pack(prediction,axis=1)
     print("packed_pred.get_shape()>>")
-    print(packed_pred.get_shape())#(input batch size),chunk_size:[last chunk]
+    print(packed_pred.get_shape())
 
 
     print("y.get_shape()>>")
-    print(y.get_shape())#(input batch size),chunk_size:[last chunk]
+    print(y.get_shape())
 
     cost = tf.reduce_sum(tf.pow(packed_pred-y, 2))
     optimizer = tf.train.AdamOptimizer().minimize(cost)
@@ -93,9 +109,11 @@ def train_neural_network(x):
 
             print('Epoch', epoch, 'completed out of',hm_epochs,'loss:',epoch_loss)
 
-        plt.plot(train_X.reshape([time_span]), train_Y.reshape([time_span]), 'b-', label='Original data')
-        plt.plot(train_X.reshape([time_span]), sess.run(packed_pred, feed_dict={x: train_X}).reshape([time_span]), 'ro', label='Original data')
-
-        plt.show()
+        for tryIdx in range(50):
+            plt.plot(train_X_b[tryIdx,:,0], 'g-', label='X data')
+            plt.plot(train_Y_b[tryIdx,:,0], 'b-', label='Ground truth Y data')
+            plt.plot(sess.run(packed_pred, feed_dict={x: train_X_b[tryIdx:tryIdx+1,:,:]}).reshape([time_span]), 'ro', label='Fitted Y data')
+            plt.legend()
+            plt.show()
 
 train_neural_network(x)
